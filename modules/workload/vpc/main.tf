f80418c0fab0b29196bba8e8551d43d5890f2059
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-05-09 20:39:45 -0300

    fix: nat gateway route

--- modules/workload/vpc/main.tf
+++ modules/workload/vpc/main.tf
@@ -51,10 +51,10 @@ resource "aws_route_table" "public" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  # route {
-  #   cidr_block     = "0.0.0.0/0"
-  #   nat_gateway_id = aws_nat_gateway.main.id
-  # }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
 
   tags = {
     Name = "rt-${var.workload}-workload-pri"
