001ba26c9951d4921ef184aad6048bc82c459a4d
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-05-09 20:49:44 -0300

    feat: cloudwatch agent

--- modules/workload/ec2/userdata/al2023.sh
+++ modules/workload/ec2/userdata/al2023.sh
@@ -3,5 +3,6 @@
 dnf check-update
 
 # CloudWatch Agent
+yum install amazon-cloudwatch-agent -y
 ssmParameterName=AmazonCloudWatch-linux-terraform
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:$ssmParameterName
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:$ssmParameterName
