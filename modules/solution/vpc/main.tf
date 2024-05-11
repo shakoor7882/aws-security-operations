55d0aaa5a327f6d59060662c7f408c70b4727803
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-05-11 11:25:08 -0300

    fix: corrected subnet routes

--- modules/solution/vpc/main.tf
+++ modules/solution/vpc/main.tf
@@ -63,7 +63,7 @@ resource "aws_route" "public_subnet_to_gateway" {
 }
 
 resource "aws_route" "private_subnet_to_nat_gateway" {
-  route_table_id         = aws_route_table.public.id
+  route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.main.id
 }
